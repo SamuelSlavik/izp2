f86d826d9d4dc500b40b0b496f83264738ce142a
Jan Spacek <janspacek56@gmail.com>
2021-11-22 22:24:15 +0100

    probably gonna fuck everything up :)

---
+++ cmake-build-debug/CMakeFiles/setcal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/setcal.dir/main.c.o"
+  "setcal"
+  "setcal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/setcal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
