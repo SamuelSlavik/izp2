ebf1299f392dd33828963ca899779a95c2be2831
samuel <samuel.slavik@protonmail.com>
2021-11-22 22:27:54 +0100

    working

---
+++ cmake-build-debug/CMakeFiles/izp2project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/izp2project.dir/main.c.o"
+  "izp2project"
+  "izp2project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/izp2project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/izp2project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for izp2project.
+# This may be replaced when dependencies are built.
