db60e7ddfa368350e7872e60f60d8b826329ccb0
Marian Valko <marianvalko01@gmail.com>
2021-11-23 19:48:30 +0100

    test

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "string.h"
+#include <string.h>
 
 typedef struct list_item {
     struct list_item *next;
@@ -65,7 +64,7 @@ void read_words(list *list,char *line){
     token = strtok(line, s);
     while (token != NULL){
         add_item(list, token);
-        printf("%s\n", token);
+        printf("%s", token);
         token = strtok(NULL, s);
     }
 }
@@ -98,33 +97,67 @@ char *readline (FILE *fp){
 }
 */
 int main(int argc, char **argv) {
+
     if (argv[1] == NULL){
-        return 2;
+        fprintf(stderr,"Error: No argument given");
+        return -1;
     }
-    printf("%d", argc);
+
+    if(argc > 2){
+        fprintf(stderr,"Error: Too much arguments");
+        return -1;
+    }
+
+    //printf("%d", argc);
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
     size_t read;
     list universum =  create_list();
+    list s = create_list();
+    list r = create_list();
     /*line = readline(fp);*/
+
     fp = fopen(argv[1], "r");
-    if (fp == NULL) return 2;
+    if (fp == NULL){
+        fprintf(stderr, "Error: Could not open file");
+        return -1;
+    } 
+    
+
+    
     char **data = NULL;
     int index = 1;
     while ((read = getline(&line, &len, fp) != -1)){
-        data[index] = malloc(sizeof (line));
-        strcpy(data[index], line);
+        
+
+        data = (char **) realloc(data, (index + 1) * sizeof(*data));
+        
+        data[index] = (char *)malloc(2 * sizeof(char));
+        
+        printf("%d", index);
+        
+        //data[index]= malloc (sizeof(char)* strlen(line));
+        //strcpy(data[index], line);
         index++;
-        printf("line is %s \n", data[index]);
+        //printf("line is %s \n", data[index]);
         switch (line[0]) {
             case 'U':
                 read_words(&universum,line);
+                //printf("%s",line);
+                //printf("%d", index);
+                
                 break;
             case 'S':
-                //read_words(&,line);
+                read_words(&s,line);
+                //printf("%d", index);
+               
                 break;
             case 'R':
+            read_words(&r,line);
+                //printf("%d", index);
+                
+                break;
 
                 break;
             case 'C':
@@ -137,11 +170,20 @@ int main(int argc, char **argv) {
 
         }
         //printf("%s\n", line);
-        read_words(&universum,line);
+        //index++;
+        //read_words(&universum,line);
     }
+    
     fclose(fp);
-    if(line) free(line);
-    free(data);
+
+    free(data[index]);
+    
+    if(line){
+        free(line);
+        
+    } 
+    
+    
     /*
     add_item(&universum, "Apple");
     add_item(&universum, "Pear");
