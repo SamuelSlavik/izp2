3b5a8e2abe2602f83cb0caacd9f8d28265fa82fc
Jan Spacek <janspacek56@gmail.com>
2021-11-26 15:46:40 +0100

    added indexing

--- main.c
+++ main.c
@@ -4,9 +4,20 @@
 #include <stdbool.h>
 #include "string.h"
 
+typedef struct{
+    char * universum;
+}uni;
+
+uni init(uni u,char *line){
+    u.universum = malloc(sizeof (char) * strlen(line));
+    strcpy(u.universum,line);
+    return u;
+}
+
 typedef struct list_item {
     struct list_item *next;
     char *value;
+    int index;
 
 } list_item;
 
@@ -16,7 +27,6 @@ typedef struct list {
 } list;
 
 typedef struct {
-    char *U;
     list R;
     list S;
     list C;
@@ -40,7 +50,12 @@ bool list_find(list *list, char *needle){
 
 void add_item(list *list, char *value){
     list_item *item = malloc(sizeof(list_item) + 1);
-    item->value = value;
+    item->value = malloc(sizeof (char)* strlen(value));
+    strcpy(item->value, value);
+    if(list->top==NULL){
+        item->index=0;
+    }
+    else item->index++;
     //printf("line is %s\n", item->value);
     list_item *current = list->top;
     if (current == NULL){
@@ -58,6 +73,7 @@ void destroy_list(list *list){
     list_item *current = list->top;
     while(current != NULL) {
         list_item  *next = current->next;
+        free(current->value);
         free(current);
         current = next;
     }
@@ -78,7 +94,21 @@ bool check_duplicates(char *substring, char *line){
     return true;
 }
 
-bool parse_words(char *line){
+// edge case what if peachtrue ???
+bool check_line(char *line){
+    for (size_t i = 0; i < sizeof (line); i++) {
+        if(line[i] >= '0' && line[i]<='9'){
+            return false;
+        }
+    }
+    if (strstr(line, "true")|| strstr(line, "false")) return false;
+    return true;
+}
+bool check_words(char *line){
+    if(!check_line(line)){
+        fprintf(stderr,"Wrong format");
+        return false;
+    }
     int counter = 0;
     unsigned long len = strlen(line);
     char *help_string = malloc(sizeof (char) * len);
@@ -88,7 +118,7 @@ bool parse_words(char *line){
     token = strtok(help_string, delim);
     while( token != NULL ) {
         counter++;
-        printf( "word is %s\n", token );
+        //printf( "word is %s\n", token );
         token = strtok(NULL, delim);
     }
     char *array[counter];
@@ -113,7 +143,7 @@ bool parse_words(char *line){
     return true;
 }
 
-bool check_set_with_uni(Dict dictionary){
+bool check_set_with_uni(Dict dictionary,uni universum){
     char *help_string = malloc(sizeof (char) * strlen(dictionary.S.top->value));
     strcpy(help_string,dictionary.S.top->value);
     const char delim[4] = " \n()";
@@ -122,18 +152,22 @@ bool check_set_with_uni(Dict dictionary){
     while( token != NULL ) {
         token = strtok(NULL, delim);
         if (token == NULL) break;
-        if((strstr(dictionary.U, token) == NULL)){
+        if((strstr(universum.universum, token) == NULL)){
             fprintf(stderr,"Word is not in universum");
             free(help_string);
             return false;
         }
     }
-    printf("sdasd %s \n",dictionary.S.top->value);
+    //printf("sdasd %s \n",dictionary.S.top->value);
     free(help_string);
     return true;
 }
 
-bool check_rel_with_uni(Dict dictionary){
+bool check_rel_with_uni(Dict dictionary, char *line,uni universum){
+    if(!check_line(line)){
+        fprintf(stderr,"Wrong format");
+        return false;
+    }
     char *help_string = malloc(sizeof (char) * strlen(dictionary.R.top->value));
     strcpy(help_string,dictionary.R.top->value);
     const char delim[4] = " \n()";
@@ -142,57 +176,159 @@ bool check_rel_with_uni(Dict dictionary){
     while( token != NULL ) {
         token = strtok(NULL, delim);
         if (token == NULL) break;
-        if((strstr(dictionary.U, token) == NULL)){
+        if((strstr(universum.universum, token) == NULL)){
             fprintf(stderr,"Word is not in universum");
             free(help_string);
             return false;
         }
     }
-    printf("sdasd %s \n",dictionary.R.top->value);
+    //printf("sdasd %s \n",dictionary.R.top->value);
+    free(help_string);
+    return true;
+}
+// mby functions will be bool, return false if parsing goes wrong i.e. empty d or empty 4(out of index)
+void empty(Dict dictionary,char *line){ printf("works");}
+void card(Dict dictionary,char *line){ printf("works");}
+void complement(Dict dictionary,char *line){ printf("works");}
+void union_f(Dict dictionary,char *line){ printf("works");}
+void intersect(Dict dictionary,char *line){ printf("works");}
+void minus(Dict dictionary,char *line){ printf("works");}
+void subseteq(Dict dictionary,char *line){ printf("works");}
+void subset(Dict dictionary,char *line){ printf("works");}
+void equals(Dict dictionary,char *line){ printf("works");}
+void reflexive(Dict dictionary,char *line){ printf("works");}
+void symmetric(Dict dictionary,char *line){ printf("works");}
+void antisymmetric(Dict dictionary,char *line){ printf("works");}
+void transitive(Dict dictionary,char *line){ printf("works");}
+void function(Dict dictionary,char *line){ printf("works");}
+void domain(Dict dictionary,char *line){ printf("works");}
+void codomain(Dict dictionary,char *line){ printf("works");}
+void injective(Dict dictionary,char *line){ printf("works");}
+void surjetive(Dict dictionary,char *line){ printf("works");}
+void bijective(Dict dictionary,char *line){ printf("works");}
+//bonus
+void closure_ref(Dict dictionary,char *line){ printf("works");}
+void closure_sym(Dict dictionary,char *line){ printf("works");}
+void closure_trans(Dict dictionary,char *line){ printf("works");}
+void select_random(Dict dictionary,char *line){ printf("works");}
+
+//NOTE: ONLY check the FIRST WORD (apart from C)
+bool check_commands(Dict dictionary, char *line){
+    char *help_string = malloc(sizeof (char) * strlen(dictionary.C.top->value));
+    strcpy(help_string,dictionary.C.top->value);
+    const char delim[4] = " ";
+    char *token;
+    token = strtok(help_string, delim);
+    token = strtok(NULL, delim);
+    if (token == NULL) {
+        free(help_string);
+        return false;
+    }
+    if (!strcmp(token,"empty")){
+        empty(dictionary,line);
+    }
+    else if (!strcmp(token,"card")){
+        card(dictionary,line);
+    }
+    else if (!strcmp(token,"complement")){
+        complement(dictionary,line);
+    }
+    else if (!strcmp(token,"union")) {
+        union_f(dictionary, line);// union is already defined function in C
+    }
+    else if (!strcmp(token,"intersect")) {
+        intersect(dictionary, line);
+    }
+    else if (!strcmp(token,"minus")) {
+        minus(dictionary, line);
+    }
+    else if (!strcmp(token,"subseteq")) {
+        subseteq(dictionary, line);
+    }
+    else if (!strcmp(token,"subset")) {
+        subset(dictionary, line);
+    }
+    else if (!strcmp(token,"equals")) {
+        equals(dictionary, line);
+    }
+    else if (!strcmp(token,"reflexive")) {
+        reflexive(dictionary, line);
+    }
+    else if (!strcmp(token,"symmetric")) {
+        symmetric(dictionary, line);
+    }
+    else if (!strcmp(token,"antisymmetric")) {
+        antisymmetric(dictionary, line);
+    }
+    else if (!strcmp(token,"transitive")) {
+        transitive(dictionary, line);
+    }
+    else if (!strcmp(token,"function")) {
+        function(dictionary, line);
+    }
+    else if (!strcmp(token,"domain")) {
+        domain(dictionary, line);
+    }
+    else if (!strcmp(token,"codomain")) {
+        codomain(dictionary, line);
+    }
+    else if (!strcmp(token,"injective")) {
+        injective(dictionary, line);
+    }
+    else if (!strcmp(token,"surjective")) {
+        surjetive(dictionary, line);
+    }
+    else if (!strcmp(token,"bijective")) {
+        bijective(dictionary, line);
+    }
+
+        //bonus functions
+    else if (!strcmp(token,"closure_sym")) {
+        closure_sym(dictionary, line);
+    }
+    else if (!strcmp(token,"closure_ref")) {
+        closure_ref(dictionary, line);
+    }
+    else if (!strcmp(token,"closure_trans")) {
+        closure_trans(dictionary, line);
+    }
+    else if (!strcmp(token,"select")) {
+        select_random(dictionary, line);//can not use select, already defined C function
+    }
+    else{
+        fprintf(stderr, "wrong command");
+        free(help_string);
+        return false;
+    }
     free(help_string);
     return true;
 }
 
-// edge case what if peachtrue ???
-bool check_line(char *line){
-    for (size_t i = 0; i < sizeof (line); i++) {
-        if(line[i] >= '0' && line[i]<='9'){
-            return false;
-        }
-    }
-    if (strstr(line, "true")|| strstr(line, "false")) return false;
-    return true;
-}
-
 int main(int argc, char **argv) {
-    printf("num of args is %d \n", argc);
+    printf("%d aby werror nejebal ze argc not used\n\n",argc);
     if (argv[1] == NULL){
-        return 5;
+        fprintf(stderr,"wrong argument");
+        return 2;
     }
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
-    Dict dictionary = {NULL,create_list(), create_list(), create_list()};
+    Dict dictionary = {create_list(), create_list(), create_list()};
     bool is_universum = false;
     bool error = false;
     fp = fopen(argv[1], "r");
-    char *help_string;
-    if (fp == NULL) return 2;
-    while ((getline(&line, &len, fp) != -1) && error==false){
+    if (fp == NULL) {
+        fprintf(stderr,"File is empty");
+        return 2;}
+    uni univ;
+    while ((getline(&line, &len, fp) != -1) && error == false){
         switch (line[0]) {
             case 'U':
                 if (!is_universum) {
-                    if(!parse_words(line)){error = true;break;}
-                    if(!check_line(line)){
-                        fprintf(stderr,"Wrong format");
-                        error = true;
-                        break;
-                    }
+                    if(!check_words(line)){ error = true;break;}
                     is_universum = true;
-                    help_string = malloc(sizeof (char) * strlen(line));
-                    strcpy(help_string,line);
-                    dictionary.U = help_string;
-                    printf("dic is working %s\n",dictionary.U);
+                    univ  = init(univ,line);
+                    printf("%s", univ.universum);
                 }
                 else{
                     fprintf(stderr,"More than 1 universum");
@@ -200,32 +336,24 @@ int main(int argc, char **argv) {
                 }
                 break;
             case 'S':
-                if(!parse_words(line)){error = true;break;}
-                if(!check_line(line)){
-                    fprintf(stderr,"Wrong format");
+                if(!check_words(line)){ error = true;break;}
+                add_item(&dictionary.S, line);
+                if(!check_set_with_uni(dictionary,univ)){
                     error = true;
                     break;
                 }
-                add_item(&dictionary.S,line);
-                printf("set is %s \n", dictionary.S.top->value);
-                if(!check_set_with_uni(dictionary)){
-                    error = true;
-                    break;
-                }
+                printf("%s \n", dictionary.S.top->value);
                 break;
             case 'R':
-                if(!check_line(line)){
-                    fprintf(stderr,"Wrong format");
-                    error = true;
-                    break;
-                }
-                add_item(&dictionary.R,line);
-                if(!check_rel_with_uni(dictionary)){ error = true;break;}
-                printf("relation is %s \n", dictionary.R.top->value);
+                add_item(&dictionary.R, line);
+                if(!check_rel_with_uni(dictionary, line,univ)){ error = true;break;}
+                printf("%s \n", dictionary.R.top->value);
                 break;
             case 'C':
-                add_item(&dictionary.C,line);
-                printf("operation is %s \n", dictionary.C.top->value);
+                add_item(&dictionary.C, line);
+                if(!check_commands(dictionary, line)){
+                    break;
+                }
                 break;
             default:
                 fprintf(stderr,"wrong identifier");
@@ -234,10 +362,10 @@ int main(int argc, char **argv) {
 
         }
     }
-
+    //check how to access items in listf
     fclose(fp);
-    if(line) free(line);
-    if (help_string) free(help_string);
+    free(line);
+    free(univ.universum);
     destroy_list(&dictionary.S);
     destroy_list(&dictionary.R);
     destroy_list(&dictionary.C);
